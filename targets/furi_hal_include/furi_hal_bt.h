036cc69acf55e2835514fc35d32ed5544dc6a846
hedger <hedger@nanode.su>
2025-06-01 16:10:52 +0100

    hal: bt: updated doxygen strings

--- targets/furi_hal_include/furi_hal_bt.h
+++ targets/furi_hal_include/furi_hal_bt.h
@@ -77,6 +77,7 @@ bool furi_hal_bt_check_profile_type(
  *
  * @param profile_template  FuriHalBleProfileTemplate instance
  * @param params            Parameters to pass to the profile. Can be NULL
+ * @param root_keys        pointer to root keys
  * @param event_cb          GapEventCallback instance
  * @param context           pointer to context
  *
@@ -101,6 +102,7 @@ void furi_hal_bt_reinit(void);
  * @param profile_template FuriHalBleProfileTemplate instance
  * @param profile_params   Parameters to pass to the profile. Can be NULL
  * @param event_cb         GapEventCallback instance
+ * @param root_keys        pointer to root keys
  * @param context          pointer to context
  *
  * @return                 instance of profile, NULL on failure
